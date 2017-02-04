f60a1480a8ee9f45e6a6279236292b809ade4ddd
raleigh wayland <rtwayland@gmail.com>
2017-02-03 22:25:10 -0700

    Dynamic li directive width

--- public/js/directives/mainNav/mainNavDirective.js
+++ public/js/directives/mainNav/mainNavDirective.js
@@ -1,13 +1,28 @@
 angular.module('app')
     .directive('mainNav', function() {
-      return {
-          restrict: 'E',
-          templateUrl: './js/directives/mainNav/main-nav.html',
-          scope: {
-            links: '='
-          },
-          link: function(scope, elem, attrs) {
+        return {
+            restrict: 'E',
+            templateUrl: './js/directives/mainNav/main-nav.html',
+            scope: {
+                links: '='
+            },
+            link: function(scope, elem, attrs) {
 
-          }
-      };
+            },
+            controller: function($scope) {
+                function getListItemWidth() {
+                    var numLinks = $scope.links.length;
+                    var width = 100 / numLinks;
+                    $scope.listItemWidth = `${width}%`;
+                }
+
+                $scope.$watch('links', (newVal, oldVal) => {
+                    if (newVal != oldVal) {
+                        getListItemWidth();
+                    }
+                })
+
+                getListItemWidth();
+            }
+        };
     });
