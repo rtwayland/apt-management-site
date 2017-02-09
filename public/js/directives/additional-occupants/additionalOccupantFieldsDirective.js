b9a8a262fc10b681a18e0eabe1a3fe3d9aa9e143
raleigh wayland <rtwayland@gmail.com>
2017-02-08 22:12:05 -0700

    Directive to dynamically create inputs

---
+++ public/js/directives/additional-occupants/additionalOccupantFieldsDirective.js
@@ -0,0 +1,15 @@
+angular.module('app')
+    .directive('additionalOccupantFields', function() {
+        return {
+            restrict: 'E',
+            templateUrl: './js/directives/additional-occupants/additional-occupant-fields.html',
+            scope: {
+                num: '='
+            },
+            link: function(scope, elem, attrs) {
+                scope.getNumOccupants = function() {
+                    return new Array(scope.num);
+                }
+            }
+        };
+    });
