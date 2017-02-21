07245acde4d287f148fc984fb162b9c2fa423b00
raleigh wayland <rtwayland@gmail.com>
2017-02-20 20:43:29 -0700

    Amazon S3 working

--- public/js/services/amazonS3.service.js
+++ public/js/services/amazonS3.service.js
@@ -1,9 +1,6 @@
 angular.module('app')
     .service('AmazonS3Service', function($http, $q) {
         this.uploadPhotos = function(photos) {
-            // var photoLinksArray = [];
-            // var defer = $q.defer();
-            console.log('Photos in Service', photos);
             photos = angular.toJson(photos);
             return $http.post('/api/upload-photos', photos)
                 .then(function(res) {
@@ -11,21 +8,5 @@ angular.module('app')
                 }, function(err) {
                     console.log(err);
                 });
-            // for (var i = 0; i < photos.length; i++) {
-            //   var singlePhoto = angular.toJson(photos[i]);
-            //   $http.post('/api/upload-photos', singlePhoto)
-            //   .then(function(res) {
-            //       console.log(res);
-            //       photoLinksArray.push('res');
-            //   }, function(err) {
-            //       console.log(err);
-            //   })
-            // }
-            //
-            // defer.resolve(photoLinksArray);
-            // return defer.promise;
-            // photos = angular.toJson(photos[0]);
-            // console.log('From Service', photos);
-            // return $http.post('/api/upload-photos', photos);
         }
     });
--- server/controllers/amazonS3.server.controller.js
+++ server/controllers/amazonS3.server.controller.js
@@ -1,51 +1,60 @@
-const AWS = require('aws-sdk'),
-    s3 = new AWS.S3(),
-    awsConfig = require('./../../awsConfig');
+const AWS = require('aws-sdk');
+AWS.config.loadFromPath('./awsJsonConfig.json');
+const s3 = new AWS.S3(),
+    awsConfig = require('./../../awsConfig'),
+    myBucket = awsConfig.bucket;
 
-const myBucket = awsConfig.bucket;
-const myKey = awsConfig.accessKey;
+var bucketParams = {
+    Bucket: myBucket
+};
+s3.createBucket(bucketParams);
+const s3Bucket = new AWS.S3({
+    params: {
+        Bucket: myBucket
+    }
+});
 
+function uploadImage(imageName, imageBody, imageType) {
+    let params = {
+        Key: imageName,
+        Body: imageBody,
+        ContentType: 'image/' + imageType,
+        ACL: 'public-read'
+    };
+    s3Bucket.putObject(params, function(err, data) {
+        if (err) {
+            console.log('Error uploading data: ', data);
+        } else {
+            console.log('Succesfully uploaded the image!');
+        }
+    });
+}
+
+function makeUniqueString() {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 8; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
 
 module.exports = {
     upload(req, res) {
-        console.log('Body', req.body);
-        // const buffer = new Buffer(req.body..replace(/^data:image\/\w+;base64,/, ""), 'base64');
-        //
-        // const bucketName = 'brettcaudill/' + req.body.userEmail;
-        // const params = {
-        //     Bucket: bucketName,
-        //     Key: req.body.imageName,
-        //     Body: buffer,
-        //     ContentType: 'image/' + req.body.imageExtension,
-        //     ACL: 'public-read'
-        // }
-        //
-        // s3.upload(params, function(err, data) {
-        //     if (err) return res.status(500).send(err);
-        //     console.log('UPLOADED:', data);
-        //     res.status(200).json(data);
-        // })
-        // s3.createBucket({
-        //     Bucket: myBucket
-        // }, function(err, data) {
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         params = {
-        //             Bucket: myBucket,
-        //             Key: myKey,
-        //             Body: 'Hello!'
-        //         };
-        //
-        //         s3.putObject(params, function(err, data) {
-        //             if (err) {
-        //                 console.log(err)
-        //             } else {
-        //                 console.log("Successfully uploaded data to myBucket/myKey");
-        //             }
-        //         });
-        //     }
-        // });
-        res.status(200).send('File(s) uploaded successfully');
+        let linkArray = [];
+        let linkPrefix = 'https://s3-us-west-2.amazonaws.com/fox-briar-properties/';
+        let photoArray = req.body;
+
+        for (let i = 0; i < photoArray.length; i++) {
+            let buffer = new Buffer(photoArray[i].imageBody.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+            let imageName = photoArray[i].imageName + makeUniqueString() + '.' + photoArray[i].imageExtension;
+
+            uploadImage(imageName, buffer, photoArray[i].imageExtension);
+
+            let linkName = linkPrefix + imageName;
+            linkArray.push(linkName);
+        }
+
+        res.status(200).json(linkArray);
     }
 }
