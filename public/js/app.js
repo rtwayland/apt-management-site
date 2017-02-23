9ce7e01b858b3a859a002d416736b27b6584dc46
raleigh wayland <rtwayland@gmail.com>
2017-02-23 14:05:23 -0700

    Add ui bootstrap depend

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngSanitize', 'ngMessages', 'ui.router', 'ngAnimate', 'ngTouch'])
+angular.module('app', ['ngSanitize', 'ngMessages', 'ui.router', 'ngAnimate', 'ngTouch', 'ui.bootstrap'])
     .run(function($rootScope) {
         $rootScope.state = 1;
     })
@@ -128,45 +128,4 @@ angular.module('app', ['ngSanitize', 'ngMessages', 'ui.router', 'ngAnimate', 'ng
                 templateUrl: './views/admin/property-details.html',
                 controller: 'PropertyDetails'
             })
-    })
-    .animation('.slide-animation', function() {
-        return {
-            beforeAddClass: function(element, className, done) {
-                var scope = element.scope();
-
-                if (className == 'ng-hide') {
-                    var finishPoint = element.parent().width();
-                    if (scope.direction !== 'right') {
-                        finishPoint = -finishPoint;
-                    }
-                    TweenMax.to(element, 0.5, {
-                        left: finishPoint,
-                        onComplete: done
-                    });
-                } else {
-                    done();
-                }
-            },
-            removeClass: function(element, className, done) {
-                var scope = element.scope();
-
-                if (className == 'ng-hide') {
-                    element.removeClass('ng-hide');
-
-                    var startPoint = element.parent().width();
-                    if (scope.direction === 'right') {
-                        startPoint = -startPoint;
-                    }
-
-                    TweenMax.fromTo(element, 0.5, {
-                        left: startPoint
-                    }, {
-                        left: 0,
-                        onComplete: done
-                    });
-                } else {
-                    done();
-                }
-            }
-        };
     });
