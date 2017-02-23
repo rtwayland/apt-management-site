07004f3300b02254a4c17e6bc9c02ae326c5d8a7
raleigh wayland <rtwayland@gmail.com>
2017-02-22 21:55:30 -0700

    First UNRESPONSIVE image slider

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngSanitize', 'ngMessages', 'ui.router'])
+angular.module('app', ['ngSanitize', 'ngMessages', 'ui.router', 'ngAnimate', 'ngTouch'])
     .run(function($rootScope) {
         $rootScope.state = 1;
     })
@@ -63,8 +63,8 @@ angular.module('app', ['ngSanitize', 'ngMessages', 'ui.router'])
             .state('logout', {
                 url: '/logout',
                 template: '',
-                controller: function ($scope, $window) {
-                  $window.location.href = '/auth/logout';
+                controller: function($scope, $window) {
+                    $window.location.href = '/auth/logout';
                 }
             })
             // ADMIN PAGES
@@ -128,4 +128,45 @@ angular.module('app', ['ngSanitize', 'ngMessages', 'ui.router'])
                 templateUrl: './views/admin/property-details.html',
                 controller: 'PropertyDetails'
             })
+    })
+    .animation('.slide-animation', function() {
+        return {
+            beforeAddClass: function(element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    var finishPoint = element.parent().width();
+                    if (scope.direction !== 'right') {
+                        finishPoint = -finishPoint;
+                    }
+                    TweenMax.to(element, 0.5, {
+                        left: finishPoint,
+                        onComplete: done
+                    });
+                } else {
+                    done();
+                }
+            },
+            removeClass: function(element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
+
+                    var startPoint = element.parent().width();
+                    if (scope.direction === 'right') {
+                        startPoint = -startPoint;
+                    }
+
+                    TweenMax.fromTo(element, 0.5, {
+                        left: startPoint
+                    }, {
+                        left: 0,
+                        onComplete: done
+                    });
+                } else {
+                    done();
+                }
+            }
+        };
     });
--- public/js/directives/image-slider/imageSlider.directive.js
+++ public/js/directives/image-slider/imageSlider.directive.js
@@ -5,15 +5,35 @@ angular.module('app')
             templateUrl: './js/directives/image-slider/image-slider.html',
             scope: {
                 photos: '='
+                // htmlID: '@'
             },
             link: function(scope, elem, attrs) {
-                $('.slider').glide({
-                    autoplay: false,
-                    arrowsWrapperClass: 'slider-arrows',
-                    arrowRightText: '',
-                    arrowLeftText: ''
-                });
+                // let id = '#' + scope.htmlID;
                 console.log(scope.photos);
+
+            },
+            controller: function($scope) {
+                $scope.direction = 'left';
+                $scope.currentIndex = 0;
+
+                $scope.setCurrentSlideIndex = function(index) {
+                    $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
+                    $scope.currentIndex = index;
+                };
+
+                $scope.isCurrentSlideIndex = function(index) {
+                    return $scope.currentIndex === index;
+                };
+
+                $scope.prevSlide = function() {
+                    $scope.direction = 'left';
+                    $scope.currentIndex = ($scope.currentIndex < $scope.photos.length - 1) ? ++$scope.currentIndex : 0;
+                };
+
+                $scope.nextSlide = function() {
+                    $scope.direction = 'right';
+                    $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.photos.length - 1;
+                };
             }
         };
     });
