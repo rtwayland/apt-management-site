7750b2763e8ae7869cbe6ba599a4e7ecb36833a8
raleigh wayland <rtwayland@gmail.com>
2017-02-23 14:08:11 -0700

    Beautify

--- public/js/directives/property-modal/propertyModalTrigger.directive.js
+++ public/js/directives/property-modal/propertyModalTrigger.directive.js
@@ -1,17 +1,16 @@
 angular.module('app')
     .directive('propertyModalTrigger', function() {
-      return {
-          restrict: 'A',
-          scope: {
-            htmlID: '=propertyModalTrigger'
-          },
-          link: function(scope, elem, attrs) {
-            elem.on('click', function(event) {
-              event.preventDefault();
-              let id = '#' + scope.htmlID;
-              $(id).modal('show');
-              console.log('Toggle modal');
-            });
-          }
-      };
+        return {
+            restrict: 'A',
+            scope: {
+                htmlID: '=propertyModalTrigger'
+            },
+            link: function(scope, elem, attrs) {
+                elem.on('click', function(event) {
+                    event.preventDefault();
+                    let id = '#' + scope.htmlID;
+                    $(id).modal('show');
+                });
+            }
+        };
     });
