303da4dd489fc6e184be3d1bb4181d7812a37935
raleigh wayland <rtwayland@gmail.com>
2017-02-27 15:00:18 -0700

    Allowing for queries

--- server/controllers/maintenance-request.server.controller.js
+++ server/controllers/maintenance-request.server.controller.js
@@ -7,12 +7,30 @@ module.exports = {
     // CRUD METHODS
     // ============================================================
     read(req, res) {
-        MaintenanceRequest.find(req.query, function(err, result) {
-            if (err) {
-                res.status(500).send(err);
-            }
-            res.status(200).send(result);
-        });
+        if (req.query.id) {
+            MaintenanceRequest.findById(req.query.id, function(err, result) {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                res.status(200).send(result);
+            });
+        } else if (req.query.status) {
+            MaintenanceRequest.find({
+                status: req.query.status
+            }, function(err, result) {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                res.status(200).send(result);
+            });
+        } else {
+            MaintenanceRequest.find(req.query, function(err, result) {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                res.status(200).send(result);
+            });
+        }
     },
     create(req, res) {
         MaintenanceRequest.create(req.body, function(err, result) {
