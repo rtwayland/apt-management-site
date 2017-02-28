14eb1fc37421af191d54b7bfb1e926c1e8facdb9
raleigh wayland <rtwayland@gmail.com>
2017-02-28 15:47:09 -0700

    Functions to edit User rent info

--- public/js/controllers/admin-users-ctrls/user-details.controller.js
+++ public/js/controllers/admin-users-ctrls/user-details.controller.js
@@ -1,16 +1,19 @@
 angular.module('app')
     .controller('UserDetails', function($scope, $state, UserService) {
         $scope.editing = false;
+        $scope.rentEdit = false;
 
         function init() {
             UserService.getUserById($state.params.id)
                 .then(function(res) {
                     console.log(res);
                     $scope.user = res;
+                    $scope.user.rentDueDate = new Date($scope.user.rentDueDate);
                 }, function(err) {
                     console.log(err);
                 })
         }
+        /**************** Edits for User Info ****************/
         $scope.edit = function() {
             $scope.editing = true;
             $scope.tempVars = {
@@ -32,5 +35,29 @@ angular.module('app')
                     console.log(err);
                 })
         }
+
+        /**************** Edits for Rent Info ****************/
+        $scope.editRent = function() {
+            $scope.rentEdit = true;
+            $scope.rentTempVars = {
+                due: $scope.user.rentDueDate,
+                amount: $scope.user.rentAmount
+            }
+        }
+        $scope.cancelEditRent = function() {
+            $scope.rentEdit = false;
+            $scope.user.rentDueDate = $scope.rentTempVars.due;
+            $scope.user.rentAmount = $scope.rentTempVars.amount;
+        }
+        $scope.saveEditRent = function() {
+            $scope.rentEdit = false;
+            UserService.updateUser($scope.user._id, $scope.user)
+                .then(function(res) {
+                    console.log(res);
+                }, function(err) {
+                    console.log(err);
+                })
+        }
+
         init();
     });
