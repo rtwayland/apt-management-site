6c2e1b333b5cb6a03b7dc7eb83aef21f6c2ea736
raleigh wayland <rtwayland@gmail.com>
2017-02-28 21:14:53 -0700

    Basic rent pay functionality

--- public/js/controllers/rent-pay.controller.js
+++ public/js/controllers/rent-pay.controller.js
@@ -1,31 +1,53 @@
 angular.module('app')
-    .controller('RentPay', function($scope, user, UserService, PaymentService) {
-        $scope.user = user;
-        console.log('User in rent', user);
-        if (!$scope.user.rentPaid) {
-            $scope.needToPayRent = true;
-        } else {
-            $scope.needToPayRent = false;
-        }
-
+    .controller('RentPay', function($scope, user, UserService) {
+        UserService.getUserById(user._id)
+            .then(function(res) {
+                console.log(res);
+                $scope.user = res;
+                if (!$scope.user.rentPaid) {
+                    $scope.needToPayRent = true;
+                } else {
+                    $scope.needToPayRent = false;
+                }
+            }, function(err) {
+                console.log(err);
+            })
+        /*********************** PAY RENT ***********************/
         $scope.payRent = function() {
             var payment = {
-                amount: '1100',
+                amount: $scope.user.rentAmount,
                 date: new Date(),
                 email: $scope.user.email,
                 userid: $scope.user._id
             }
-            // PaymentService.addPayment(payment)
-            //     .then(function(res) {
+
+            $scope.user.rentDueDate = incrementDueDate($scope.user.rentDueDate);
+            $scope.user.rentPaid = true;
+
+            UserService.updateUser($scope.user._id, $scope.user)
+                .then(function(res) {
                     UserService.payRent($scope.user._id, payment)
                         .then(function(res) {
                             console.log(res);
+                            $scope.user = res;
+                            $scope.needToPayRent = false;
                         }, function(err) {
                             console.log(err);
                         })
-                // }, function(err) {
-                //     console.log(err);
-                // })
+                }, function(err) {
+                    console.log(err);
+                })
 
+
+        }
+
+        /*********************** INCREMENT DUE DATE ***********************/
+        function incrementDueDate(date) {
+            let now = new Date(date);
+            if (now.getMonth() == 11) {
+                return new Date(now.getFullYear() + 1, 0, 1);
+            } else {
+                return new Date(now.getFullYear(), now.getMonth() + 1, 1);
+            }
         }
     });
