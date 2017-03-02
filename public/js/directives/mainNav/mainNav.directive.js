114cb726551b266152febadf902b3e8380f2fd3d
raleigh wayland <rtwayland@gmail.com>
2017-03-02 15:45:19 -0700

    Adding admin home link

--- public/js/directives/mainNav/mainNav.directive.js
+++ public/js/directives/mainNav/mainNav.directive.js
@@ -12,80 +12,84 @@ angular.module('app')
                 }
             },
             controller: function($scope) {
-              var publicNavLinks = [{
-                      link: 'available-properties',
-                      name: 'Availabilities'
-                  },
-                  {
-                      link: 'apply',
-                      name: 'Apply Now'
-                  },
-                  {
-                      link: 'contact',
-                      name: 'Contact Us'
-                  },
-                  {
-                      link: 'resident-login',
-                      name: 'Resident Login'
-                  }
-              ];
-              var residentNavLinks = [{
-                      link: 'resident-maintenance',
-                      name: 'Maintenance Request'
-                  },
-                  {
-                      link: 'resident-rent',
-                      name: 'Pay Rent'
-                  },
-                  {
-                      link: 'logout',
-                      name: 'Log Out'
-                  }
-              ];
-              var adminNavLinks = [{
-                      link: 'applications',
-                      name: 'Applications'
-                  },
-                  {
-                      link: 'admin-maintenance',
-                      name: 'Maintenance Requests'
-                  },
-                  {
-                      link: 'tenants',
-                      name: 'Tenants'
-                  },
-                  {
-                      link: 'properties',
-                      name: 'Properties'
-                  },
-                  {
-                      link: 'logout',
-                      name: 'Log Out'
-                  }
-              ];
+                var publicNavLinks = [{
+                        link: 'available-properties',
+                        name: 'Availabilities'
+                    },
+                    {
+                        link: 'apply',
+                        name: 'Apply Now'
+                    },
+                    {
+                        link: 'contact',
+                        name: 'Contact Us'
+                    },
+                    {
+                        link: 'resident-login',
+                        name: 'Resident Login'
+                    }
+                ];
+                var residentNavLinks = [{
+                        link: 'resident-maintenance',
+                        name: 'Maintenance Request'
+                    },
+                    {
+                        link: 'resident-rent',
+                        name: 'Pay Rent'
+                    },
+                    {
+                        link: 'logout',
+                        name: 'Log Out'
+                    }
+                ];
+                var adminNavLinks = [{
+                        link: 'admin',
+                        name: 'Admin Home'
+                    },
+                    {
+                        link: 'applications',
+                        name: 'Applications'
+                    },
+                    {
+                        link: 'admin-maintenance',
+                        name: 'Maintenance Requests'
+                    },
+                    {
+                        link: 'tenants',
+                        name: 'Tenants'
+                    },
+                    {
+                        link: 'properties',
+                        name: 'Properties'
+                    },
+                    {
+                        link: 'logout',
+                        name: 'Log Out'
+                    }
+                ];
 
-              function updateNav() {
-                  switch ($scope.state) {
-                      case 1:
-                          $scope.links = publicNavLinks;
-                          break;
-                      case 2:
-                          $scope.links = residentNavLinks;
-                          break;
-                      case 3:
-                          $scope.links = adminNavLinks;
-                          break;
-                      default:
-                          $scope.links = publicNavLinks;
-                          break;
-                  }
-              }
+                function updateNav() {
+                    switch ($scope.state) {
+                        case 1:
+                            $scope.links = publicNavLinks;
+                            break;
+                        case 2:
+                            $scope.links = residentNavLinks;
+                            break;
+                        case 3:
+                            $scope.links = adminNavLinks;
+                            break;
+                        default:
+                            $scope.links = publicNavLinks;
+                            break;
+                    }
+                }
 
-              $scope.$watch('state', () => {
-                  updateNav();
-              });
+                $scope.$watch('state', () => {
+                    updateNav();
+                });
 
-              updateNav();
+                updateNav();
             }
         };
     });
